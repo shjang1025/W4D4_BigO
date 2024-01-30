a6433e49eb3bb1a2e5074d6308ab12778b9d5bcf
Tyler Johnson <tylerj0937@gmail.com>
2024-01-29 18:30:34 -0800

    finish anagrams

--- practice-for-ch-ruby-anagrams-long-practice-main/anagrams.rb
+++ practice-for-ch-ruby-anagrams-long-practice-main/anagrams.rb
@@ -8,16 +8,28 @@ end
 
 #Phase 2
 def second_anagram?(str1, str2)
-    ana_1 = str1.split("")
-    ana_2 = str2.split("")
+  ana_1 = str1.split("")
+  ana_2 = str2.split("")
 
-    ana_2.each do |char|
+  ana_1.each do |char|
+    if ana_2.index(char) == nil
+      return false
+    else
+      i = ana_2.index(char)
+      ana_2.delete_at(i)
+    end
+  end
+  ana_2.empty?
+end
+ 
 
-    if ana_2.find_index(ana_1) != nil
+def third_anagram?(str1, str2)
+  str1.chars.sort == str2.chars.sort
+end
 
-    # "elvis".split("")
-    # => ["e", "l", "v", "i", "s"]
-
-    # "lives".split("")
-    # => ["l", "i", "v", "e", "s"]
+def fourth_anagram?(str1, str2)
+  hash1 = Hash.new(0)
+  str1.each_char {|char| hash1[char] += 1}
+  str2.each_char {|char| hash1[char] -= 1}
+  hash1.values.all?(0)
 end
